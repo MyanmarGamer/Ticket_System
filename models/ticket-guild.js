02f1b71e7bd26cd7eb8a464c44566aac1c6b3ff4
Zedro2742 <61875966+Zedro2742@users.noreply.github.com>
2022-01-22 21:15:02 +0530

    Update ticket-guild.js

--- models/ticket-guild.js
+++ models/ticket-guild.js
@@ -9,3 +9,13 @@ const Tickets = new Schema({
 })
 
 module.exports = model("ticket-guild", Tickets);
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by Zedro#2742 | https://discord.gg/milanio
+ * @INFO
+ * Work for Milanio Development | https://discord.gg/milanio
+ * @INFO
+ * Please Mention Us Milanio Development, When Using This Code!
+ * @INFO
+ *********************************************************/
