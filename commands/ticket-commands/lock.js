930b238f8d4145fbbced665b50d54ef4ce75f632
Zedro2742 <61875966+Zedro2742@users.noreply.github.com>
2022-01-22 21:22:41 +0530

    Update lock.js

--- commands/ticket-commands/lock.js
+++ commands/ticket-commands/lock.js
@@ -95,3 +95,14 @@ module.exports = {
         }
     }
 }
+
+
+/**********************************************************
+ * @INFO
+ * Bot Coded by Zedro#2742 | https://discord.gg/milanio
+ * @INFO
+ * Work for Milanio Development | https://discord.gg/milanio
+ * @INFO
+ * Please Mention Us Milanio Development, When Using This Code!
+ * @INFO
+ *********************************************************/
